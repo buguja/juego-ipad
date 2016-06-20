2f5b3d377d1e76914cad4f113611e78ff061088e
buguja <abrew_javier@hotmail.com>
2016-06-20 14:40:55 -0500

    Apoyo para responder el examen parcial 3

---
+++ SimpleGLKitGame/SGGViewController.h
@@ -0,0 +1,15 @@
+//
+//  SGGViewController.h
+//  SimpleGLKitGame
+//
+//  Created by Ray Wenderlich on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface SGGViewController : GLKViewController
+- (IBAction)cerrar:(id)sender;
+- (IBAction)pause:(id)sender;
+
+@end
